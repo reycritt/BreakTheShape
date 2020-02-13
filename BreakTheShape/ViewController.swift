26032ec5702314bbe82baac336ae54eb77ac47b2
Rey <reycrittenden@yahoo.com>
2020-02-12 23:57:02 -0600

    Testing the movement and interaction of a button

--- BreakTheShape/ViewController.swift
+++ BreakTheShape/ViewController.swift
@@ -10,9 +10,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var projectile: UIButton!
+    var timer: Timer?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        //timer = Timer.scheduledTimer(withTimeInterval: 2, repeats: true, block: projectileMove(_:))
+        timer = Timer.scheduledTimer(timeInterval: 2, target: self, selector: #selector(projectileMove(_:)), userInfo: nil, repeats: true)
+    }
+    
+    @IBAction func projectilePopped(_ sender: Any) {
+        //timer?.invalidate()
+        //self.projectile.frame.origin.y -= 200
+        UIView.animate(withDuration: 0, animations: {
+            self.projectile.frame.origin.y += 200
+        })
+    }
+    
+    @objc func projectileMove(_ sender: Any){
+        //UIView.animate(withDuration: 0, animations: {
+            //self.projectile.frame.origin.y += 10
+        //})
     }
 
 
